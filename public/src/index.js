9fdadedf87ce378f7b0f2c99b8bbf2e102b39e33
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-10-03 01:17:48 +0700

    .

--- public/src/index.js
+++ public/src/index.js
@@ -6,11 +6,47 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  NavLink
 } from "react-router-dom";
+
+import {AppBar, Box, Toolbar, Button, Typography, ButtonGroup} from '@mui/material'
 
 ReactDOM.render(
   <Router>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar >
+          <Typography variant="h4" style={{ border:"200px"}}>
+            Folktale
+          </Typography>
+
+          <ButtonGroup variant="text" aria-label="large secondary button group" sx={{ flexGrow: 1 }}>
+            <Button color="inherit">
+              <NavLink to="/home" activeClassName="selected" style={{ textDecoration: 'none' }}>
+                Home
+              </NavLink>
+            </Button>
+            
+            <Button color="inherit">
+              <NavLink to="/blogs" activeClassName="selected" style={{ textDecoration: 'none' }}>
+                Blogs
+              </NavLink>
+            </Button>
+
+            <Button color="inherit">
+              <NavLink to="/about" activeClassName="selected" style={{ textDecoration: 'none' }}>
+                About Us
+              </NavLink>
+            </Button>
+          </ButtonGroup>
+          
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+
+
     <Switch>
       <Route path="/">
         <App />
