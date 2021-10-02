51a4be8162cbc9e6178f59fabb39cafb5e319e23
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-10-02 22:31:45 +0700

    create react app

---
+++ public/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
+
