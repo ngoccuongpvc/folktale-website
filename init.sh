6337c80c7bbee82c1926ee7346962285c26d3920
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-10-02 23:58:18 +0700

    fix dynamic port

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+if [[ -z "${PORT}" ]]; then
+  LISTENING_PORT="80"
+else
+  LISTENING_PORT="${PORT}"
+fi
+
+sed -i -e 's/$PORT/'"$LISTENING_PORT"'/g' /etc/nginx/nginx.conf
+
 /app/main &
 nginx -g 'daemon off;'
