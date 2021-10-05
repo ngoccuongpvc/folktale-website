4eed462569b10445b1446ce7cbf2cd32eae08671
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-10-06 00:40:56 +0700

    .

--- public/src/routes/Blogs/Blogs.js
+++ public/src/routes/Blogs/Blogs.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Editor from '@react-page/editor';
+import '@react-page/editor/lib/index.css';
+
 import { imagePlugin } from '@react-page/plugins-image';
+import '@react-page/plugins-image/lib/index.css';
 
 import slate from '@react-page/plugins-slate';
+import '@react-page/plugins-slate/lib/index.css';
+
 import spacerPlugin from '@react-page/plugins-spacer';
 import '@react-page/plugins-spacer/lib/index.css';
 
@@ -11,14 +16,19 @@ import backgroundPlugin, { ModeEnum } from '@react-page/plugins-background';
 import '@react-page/plugins-background/lib/index.css';
 
 import videoPlugin from '@react-page/plugins-video';
+import '@react-page/plugins-video/lib/index.css';
 
 import html5videoPlugin from '@react-page/plugins-html5-video';
 import '@react-page/plugins-html5-video/lib/index.css';
 
-import '@react-page/editor/lib/index.css';
-
 const uploadHandler = (file, progress) => {
     console.log(file)
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(resolve)
+          resolve({ url: 'https://firebasestorage.googleapis.com/v0/b/folktale-8a942.appspot.com/o/image1.png?alt=media&token=3b78aab8-ab1f-4972-8118-3d160311b506' });
+        }, 2000);
+      });
 }
 
 const cellPlugins = [
