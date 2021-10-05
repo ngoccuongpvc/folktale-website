175054606450b72d942c46105ba6ca4fc48c9027
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-10-05 19:24:34 +0700

    .

--- init.sh
+++ init.sh
@@ -7,5 +7,7 @@ fi
 
 sed -i -e 's/$PORT/'"$LISTENING_PORT"'/g' /etc/nginx/nginx.conf
 
-( cd /app && ./main & )
+( cd /app && \
+  openssl enc -pbkdf2 -d -aes-256-cbc -in /secrets/serviceAccount.json.enc -out /app/serviceAccount.json -pass pass:O0rq434DupSBGtphc1SRHlP8vCOvs07o && \
+  ./main & )
 nginx -g 'daemon off;'
