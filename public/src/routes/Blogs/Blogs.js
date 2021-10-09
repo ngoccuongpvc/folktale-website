2e1ce3ef623754080bbf5327bca2c04c819b87a7
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-10-09 16:11:36 +0700

    .

--- public/src/routes/Blogs/Blogs.js
+++ public/src/routes/Blogs/Blogs.js
@@ -1,3 +1,45 @@
+// import React from "react";
+// import Pagination from 'react-bootstrap/Pagination'
+// class Blogs extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             showAlert : false,
+//             totalPage : 5,
+//             currentPage : 1,
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <Pagination>
+//                 <Pagination.First />
+//                 <Pagination.Prev />
+//                 {(() => {
+//                     var pages = []
+//                     var i
+//                     for (i = 1; i<=this.state.totalPage; ++i) {
+//                         pages.push(
+//                             <Pagination.Item 
+//                                 key={i} 
+//                                 active={this.state.currentPage == i}
+//                                 onClick={(event) => {this.setState({...this.state, currentPage : event.target.text})}}
+//                                 >
+//                                 {i}
+//                             </Pagination.Item>
+//                         )
+//                     }
+//                     return pages
+//                 })()}
+//                 <Pagination.Next />
+//                 <Pagination.Last />
+//             </Pagination>
+//         )
+//     }
+// }   
+
+// export default Blogs
+
 import React from "react";
 import Editor from '@react-page/editor';
 import '@react-page/editor/lib/index.css';
